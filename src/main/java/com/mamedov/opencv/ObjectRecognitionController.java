c4981385000eb036ae44e7f5f4b86490aa770843
Vagif <74174732+Mamedov14@users.noreply.github.com>
2022-04-13 19:32:12 +0300

    update

--- src/main/java/com/mamedov/opencv/ObjectRecognitionController.java
+++ src/main/java/com/mamedov/opencv/ObjectRecognitionController.java
@@ -11,12 +11,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -359,15 +368,181 @@ public class ObjectRecognitionController {
         BorderPane root = new BorderPane(); // Корневой контейнер, в него помещаются кнопка для выбора и само изображение.
         root.setCenter(imageNotSelected); // Так как изображение не выбрано отображаем текст 'Изображение не выбрано'
         ImageView imageView = new ImageView(); // Данный компонент показывает выбранное изображение.
-        Button button = new Button("Select image"); // Кнопка для выбора изображения.
+        Button button = new Button("Set Image"); // Кнопка для выбора изображения.
         BorderPane.setAlignment(button, Pos.CENTER); // Выравнивание кнопки по середине.
         root.setBottom(button); // Добавление кнопки в контейнер.
         button.setPadding(new Insets(10));
         button.setStyle("-fx-padding: 10");
+        Image image = chooser.openImage();
+//        addImageView(chooser, imageNotSelected, root, imageView, button);
+        button.setOnAction((event) -> { // Обработчик событий для нажатия кнопки.
+            // Выбираем изображение.
+            if (image != null) {
+                imageView.setImage(image); // Установка изображения.
+                imageView.setFitWidth(500.0); // Установка ширины в 100.0.
+                imageView.setFitHeight(500.0); // Установка высоты в 100.0.
+                root.setCenter(imageView); // Добавление изображения в контейнер.
+            } else
+                root.setCenter(imageNotSelected); // Если изображение не выбрано, тогда показываем 'Image not Selected'
+        });
 
-        addImageView(chooser, imageNotSelected, root, imageView, button);
-        newStage(root);
+        Button originalButton = new Button("Original Image");
+        Button bgrButton = new Button("BGR Image");
+        Button grayButton = new Button("Gray Image");
+        Button hsvButton = new Button("HSV Image");
+
+        VBox box = new VBox(10, originalButton, bgrButton, grayButton, hsvButton);
+        box.setAlignment(Pos.CENTER);
+
+        originalButton.setOnAction((event) -> {
+
+        });
+
+        bgrButton.setOnAction((event) -> {
+            Mat bgrImage = new Mat();
+            Imgproc.cvtColor(imageToMat(image), bgrImage, Imgproc.COLOR_RGB2BGR);
+//            Mat img = Imgcodecs.imread("Z:\\image.jpg");
+            if (bgrImage.empty()) {
+                System.out.println("Failed to load image");
+                return;
+            }
+            showImage(bgrImage, "BGR Image");
+        });
+
+        grayButton.setOnAction((event) -> {
+
+        });
+
+        hsvButton.setOnAction((event) -> {
+
+        });
+
+
+        root.setRight(box);
+
+        Stage primaryStage = new Stage();
+        Scene scene = new Scene(root, 640.0, 480.0); // Создание сцены.
+        primaryStage.setScene(scene); // Установка сцены.
+        primaryStage.setTitle("Add Image");
+        primaryStage.show(); // Показываем окно
+
+//        newStage(root);
     }
+
+    private static Mat imageToMat(Image image) {
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        byte[] buffer = new byte[width * height * 4];
+
+        PixelReader reader = image.getPixelReader();
+        WritablePixelFormat<ByteBuffer> format = WritablePixelFormat.getByteBgraInstance();
+        reader.getPixels(0, 0, width, height, format, buffer, 0, width * 4);
+
+        Mat mat = new Mat(height, width, CvType.CV_8UC4);
+        mat.put(0, 0, buffer);
+        return mat;
+    }
+
+    public static Image mat2Image(Mat frame) {
+        // create a temporary buffer
+        MatOfByte buffer = new MatOfByte();
+        // encode the frame in the buffer, according to the PNG format
+        Imgcodecs.imencode(".png", frame, buffer);
+        // build and return an Image created from the image encoded in the
+        // buffer
+        return new Image(new ByteArrayInputStream(buffer.toArray()));
+
+    }
+
+//    private Image mat2Image(Mat src) {
+////        BufferedImage image = ImageConverter.toImage(src);
+////        return SwingFXUtils.toFXImage(/*image*/, null);
+//    }
+
+
+    public static void showImage(Mat img, String title) {
+        BufferedImage im = MatToBufferedImage(img);
+        if (im == null) {
+            return;
+        }
+        int w = 1000, h = 600;
+        JFrame window = new JFrame(title);
+        window.setSize(w, h);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon imageIcon = new ImageIcon(im);
+        JLabel label = new JLabel(imageIcon);
+        JScrollPane pane = new JScrollPane(label);
+        window.setContentPane(pane);
+        if (im.getWidth() < w && im.getHeight() < h) {
+            window.pack();
+        }
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+    //  Mat To BufferedImage.
+
+    public static BufferedImage MatToBufferedImage(Mat mat) {
+
+        if (mat == null || mat.empty()) {
+            return null;
+        }
+        if (mat.depth() == CvType.CV_8U) {
+        } else if (mat.depth() == CvType.CV_16U) { // CV_16U => CV_8U
+            Mat m_16 = new Mat();
+            mat.convertTo(m_16, CvType.CV_8U, 255.0 / 65535);
+            mat = m_16;
+        } else if (mat.depth() == CvType.CV_32F) { // CV_32F => CV_8U
+            Mat m_32 = new Mat();
+            mat.convertTo(m_32, CvType.CV_8U, 255);
+            mat = m_32;
+        } else {
+            return null;
+        }
+        int type = 0;
+        if (mat.channels() == 1)
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        else if (mat.channels() == 3)
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        else if (mat.channels() == 4)
+            type = BufferedImage.TYPE_4BYTE_ABGR;
+        else {
+            return null;
+        }
+        byte[] buf = new byte[mat.channels() * mat.cols() * mat.rows()];
+        mat.get(0, 0, buf);
+        byte temp = 0;
+        if (mat.channels() == 4) { // BGRA => ABGR
+            for (int i = 0; i < buf.length; i += 4) {
+                temp = buf[i + 3];
+                buf[i + 3] = buf[i + 2];
+                buf[i + 2] = buf[i + 1];
+                buf[i + 1] = buf[i];
+                buf[i] = temp;
+            }
+        }
+        BufferedImage image = new BufferedImage(mat.cols(), mat.rows(), type);
+        byte[] data =
+                ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(buf, 0, data, 0, buf.length);
+        return image;
+    }
+
+    private static BufferedImage toBufferedImage(Mat matrix) {
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (matrix.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = matrix.channels() * matrix.cols() * matrix.rows();
+        byte[] buffer = new byte[bufferSize];
+        matrix.get(0, 0, buffer);
+        // Получаем все пиксели
+        BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(buffer, 0, targetPixels, 0, buffer.length);
+        return image;
+    }
+
 
     private void newStage(BorderPane root) {
         Stage primaryStage = new Stage();
